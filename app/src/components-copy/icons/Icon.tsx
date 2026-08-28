3754a0acaf02b0d0e036d7cbe94d05bb3c2ec53e
Seth Foster <seth@opentrons.com>
2026-08-28 14:30:08 -0400

    chore: vendor a slice of opentrons-components
    Yes, this is copied code from the monorepo. The reason is that even
    though we do publish the components library to npm, it has a dependency
    of styled-components, which otherwise isn't used at all and will
    increasingly not work with react. For now, just copy-paste and fix only
    what's necessary to get ts6 to work; the goal is that eventually we'll
    be able to replace this with a normal import with minimal pain.

---
+++ app/src/components-copy/icons/Icon.tsx
@@ -0,0 +1,90 @@
+import clsx from "clsx"
+import type { ReactNode, SVGProps } from "react"
+
+import { withStyleProps } from "../hocs/withStyleProps"
+import { ICON_DATA_BY_NAME } from "./icon-data"
+
+import styles from "./icon.module.css"
+
+export type IconName = keyof typeof ICON_DATA_BY_NAME
+
+export interface IconProps extends SVGProps<SVGSVGElement> {
+  /** name constant of the icon to display */
+  name: IconName
+  /** spin the icon with a CSS animation */
+  spin?: boolean
+  /** override default size */
+  size?: string | number
+  /** x attribute as a number or string (for nesting inside another SVG) */
+  x?: number | string
+  /** y attribute as a number or string (for nesting inside another SVG) */
+  y?: number | string
+  /** width as a number or string (for nesting inside another SVG) */
+  svgHeight?: number | string
+  /** height as a number or string (for nesting inside another SVG) */
+  svgWidth?: number | string
+  /** inline style passed into the icon svg */
+  style?: Record<string, string | number>
+  /** optional children */
+  children?: ReactNode
+}
+
+/**
+ * Inline SVG icon component
+ *
+ * If you need access to the IconName type, you can:
+ * ```js
+ * import type { IconName } from '@opentrons/components'
+ * ```
+ */
+function IconComponent(props: IconProps): ReactNode {
+  const {
+    name,
+    className,
+    spin,
+    size,
+    height: rawHeight,
+    width: rawWidth,
+    color,
+    transform,
+    opacity: _opacity,
+    ...svgProps
+  } = props
+
+  const height = size ?? rawHeight
+  const width = size ?? rawWidth
+  if (!(name in ICON_DATA_BY_NAME)) {
+    console.error(`"${name}" is not a valid Icon name`)
+    return null
+  }
+
+  const { viewBox, path } = ICON_DATA_BY_NAME[name]
+
+  const style = Object.fromEntries(
+    Object.entries({
+      color,
+      height,
+      width,
+      transform,
+      //  filter undefined props
+    }).filter(([_, value]) => value != null),
+  )
+
+  return (
+    <svg
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox={viewBox}
+      className={clsx(className, { [styles.spin]: spin })}
+      // need this because this component needs to allow engineers to make some style overrides
+      // eslint-disable-next-line react/forbid-dom-props
+      style={{ ...style }}
+      {...svgProps}
+    >
+      <path aria-roledescription={name} fillRule="evenodd" d={path} />
+      {props.children}
+    </svg>
+  )
+}
+
+export const Icon = withStyleProps(IconComponent)
