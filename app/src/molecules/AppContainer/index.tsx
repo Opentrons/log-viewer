4fb601064ae083248c98de5c08a6dcd7fbfa8b8f
Seth Foster <seth@opentrons.com>
2026-09-04 15:50:15 -0400

    fix: border changes and little FE fixes
    We need some borders and they need to not stack up, and we're going to
    have regular content in the mainwindow soon shift around whos'e
    responsible for borders and background colors.
    
    Also remove some unused components and twiddle some children props.

--- app/src/molecules/AppContainer/index.tsx
+++ app/src/molecules/AppContainer/index.tsx
@@ -3,9 +3,9 @@ import * as React from "react"
 import styles from "./appcontainer.module.css"
 
 export interface AppContainerProps {
-  children: React.ReactNode[]
+  children: React.ReactNode
 }
 
 export function AppContainer(props: AppContainerProps): React.ReactNode {
-  return <div className={styles.app_container}>{...props.children}</div>
+  return <div className={styles.app_container}>{props.children}</div>
 }
