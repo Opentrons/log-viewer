e2f1817707f88dc54b007ba4c21cb8a1331f4f2f
Seth Foster <seth@opentrons.com>
2026-09-10 15:02:24 -0400

    feat(app): chip wrapper for log status
    This is a chip but with mappings to log verifier semantics.

---
+++ app/src/atoms/LogStatus/index.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+
+import { Chip } from "@/components-copy/atoms/Chip"
+import type { StyleProps } from "@/components-copy/primitives/types"
+import type { ConsistencyStatus } from "@/redux/logDirectory/types"
+export interface LogStatusProps extends StyleProps {
+  status: ConsistencyStatus
+}
+
+const STATUS_TO_TYPE = {
+  unverified: "warning",
+  consistent: "success",
+  inconsistent: "error",
+} as const
+
+const STATUS_TO_ICON = {
+  unverified: "error",
+  inconsistent: "error",
+  consistent: "check-circle",
+} as const
+
+const STATUS_TO_TEXT = {
+  unverified: "Unknown ID",
+  inconsistent: "Fail",
+  consistent: "Pass",
+}
+
+export function LogStatus({ status, ...styleProps }: LogStatusProps): React.ReactNode {
+  return (
+    <Chip
+      background
+      chipSize="small"
+      type={STATUS_TO_TYPE[status]}
+      iconName={STATUS_TO_ICON[status]}
+      hasIcon
+      text={STATUS_TO_TEXT[status]}
+      {...styleProps}
+    />
+  )
+}
