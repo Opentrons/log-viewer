bee8e318f80a67aea7247ed5e9cc8544150fb90c
Seth Foster <seth@opentrons.com>
2026-09-21 14:44:04 -0400

    feat(app): display log status banner
    This goes in the log period overview top panel. It shows the status of
    the period as a whole.

--- app/src/organisms/LogPeriodOverview/index.tsx
+++ app/src/organisms/LogPeriodOverview/index.tsx
@@ -1,5 +1,7 @@
+import countBy from "lodash/countBy"
 import * as React from "react"
 
+import { InlineNotification } from "@/components-copy/atoms/InlineNotification"
 import { MenuList } from "@/components-copy/atoms/MenuList"
 import { MenuItem } from "@/components-copy/atoms/MenuList/MenuItem"
 import { OverflowBtn } from "@/components-copy/atoms/MenuList/OverflowBtn"
@@ -7,6 +9,7 @@ import { useOnClickOutside } from "@/components-copy/interaction-enhancers"
 import { LogPeriodTopPanel } from "@/molecules/LogPeriodTopPanel"
 import { LogEntries } from "@/organisms/LogEntries"
 import type { SelectedLogPeriod } from "@/redux/logDirectory/hooks"
+import { useFilteredLogs } from "@/redux/logDirectory/hooks"
 
 import style from "./logperiodoverview.module.css"
 
@@ -14,7 +17,42 @@ export interface LogPeriodOverviewProps {
   logPeriod: SelectedLogPeriod
 }
 
-export function LogPeriodOverview(props: LogPeriodOverviewProps): React.ReactNode {
+const bannerPropsFromConsistency = (
+  logPeriod: SelectedLogPeriod,
+): React.ComponentProps<typeof InlineNotification> | null => {
+  const logLines = useFilteredLogs()
+  if ((logPeriod?.sequentialConsistency?.status ?? "unverified") === "unverified") {
+    return null
+  }
+
+  if (logPeriod.sequentialConsistency.status !== "consistent") {
+    const errorCount =
+      logLines.status === "loaded"
+        ? logLines.lines.length -
+          countBy(logLines.lines, (line) => line.sequentialConsistency.status)["consistent"]
+        : 0
+    return {
+      type: "error",
+      message: `${errorCount} ${errorCount === 1 ? "issue" : "issues"} detected`,
+    }
+  } else if (logPeriod.attestationConsistency.status !== "consistent") {
+    return { type: "alert", message: "Robot identity is unknown" }
+  } else {
+    return { type: "success", message: "All log entries verified" }
+  }
+}
+
+function LogPeriodStatusBanner({ logPeriod }: LogPeriodOverviewProps): React.ReactNode | null {
+  const notificationProps = bannerPropsFromConsistency(logPeriod)
+
+  return notificationProps == null ? null : (
+    <div className={style.banner_container}>
+      <InlineNotification {...notificationProps} />
+    </div>
+  )
+}
+
+export function LogPeriodOverview({ logPeriod }: LogPeriodOverviewProps): React.ReactNode {
   const [showOverflowMenu, setShowOverflowMenu] = React.useState<boolean>(false)
   const overflowWrapperRef = useOnClickOutside<HTMLDivElement>({
     onClickOutside: () => {
@@ -22,34 +60,37 @@ export function LogPeriodOverview(props: LogPeriodOverviewProps): React.ReactNod
     },
   })
   return (
-    <div className={style.container}>
-      <div className={style.overflow_button_clipper}>
-        <div className={style.overflow_button_container}>
-          <OverflowBtn
-            onClick={() => {
-              setShowOverflowMenu(true)
+    <div className={style.outer_container}>
+      <LogPeriodStatusBanner logPeriod={logPeriod} />
+      <div className={style.container}>
+        <div className={style.overflow_button_clipper}>
+          <div className={style.overflow_button_container}>
+            <OverflowBtn
+              onClick={() => {
+                setShowOverflowMenu(true)
+              }}
+            />
+          </div>
+        </div>
+        {showOverflowMenu && (
+          <div
+            className={style.overflow_menu_container}
+            ref={overflowWrapperRef}
+            onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+              e.preventDefault()
+              e.stopPropagation()
             }}
-          />
-        </div>
+            role="presentation"
+          >
+            <MenuList>
+              <MenuItem>View robot ID</MenuItem>
+              <MenuItem>Show associated files</MenuItem>
+            </MenuList>
+          </div>
+        )}
+        <LogPeriodTopPanel logPeriod={logPeriod} />
+        <LogEntries logPeriod={logPeriod} />
       </div>
-      {showOverflowMenu && (
-        <div
-          className={style.overflow_menu_container}
-          ref={overflowWrapperRef}
-          onClick={(e: React.MouseEvent<HTMLDivElement>) => {
-            e.preventDefault()
-            e.stopPropagation()
-          }}
-          role="presentation"
-        >
-          <MenuList>
-            <MenuItem>View robot ID</MenuItem>
-            <MenuItem>Show associated files</MenuItem>
-          </MenuList>
-        </div>
-      )}
-      <LogPeriodTopPanel logPeriod={props.logPeriod} />
-      <LogEntries logPeriod={props.logPeriod} />
     </div>
   )
 }
