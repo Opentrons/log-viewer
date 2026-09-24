1cb79c4a2444a540add971ad3b1e9d12e94eecfb
Seth Foster <seth@opentrons.com>
2026-09-24 09:48:35 -0400

    chore: remove modals

--- app/src/App/modals.ts
+++
@@ -1,7 +0,0 @@
-import * as NiceModal from "@ebay/nice-modal-react"
-
-import { TrustRobotIdentityModal } from "@/organisms/TrustRobotIdentityModal"
-
-export function registerModals(): void {
-  NiceModal.register("TrustRobotIdentityModal", TrustRobotIdentityModal)
-}
